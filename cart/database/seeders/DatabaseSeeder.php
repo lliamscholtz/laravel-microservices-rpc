d283658b674cdf33f35a7d423620e42e461b1250
Lliam <lliam@plusnarrative.com>
2024-03-26 14:07:08 +0200

    chore: consistent product sku seeders

--- cart/database/seeders/DatabaseSeeder.php
+++ cart/database/seeders/DatabaseSeeder.php
@@ -14,6 +14,14 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         Cart::factory(3)->create();
-        CartProduct::factory(3)->create();
+        CartProduct::factory()->create([
+            'product_sku' => 'd62bfa6e-a67b-35e2-899e-857f31d3b528',
+        ]);
+        CartProduct::factory()->create([
+            'product_sku' => 'a7b36603-dd9e-364a-aa83-cfe691739168',
+        ]);
+        CartProduct::factory()->create([
+            'product_sku' => '04521b83-8064-3862-a20c-815222502659',
+        ]);
     }
 }
